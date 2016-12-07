5e6a5de512c3e05852fc4a6540f04f6283abe924
IvanGiron <ivansioncss@gmail.com>
2016-12-07 18:20:26 +0100

    Montado 2 ejercicio probando el script

---
+++ Assets/Scripts/CarroSurfero.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarroSurfero : MonoBehaviour {
+	private DistanceJoint2D conector;
+
+	// Use this for initialization
+	void Start () {
+		conector = GetComponent<DistanceJoint2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+
+	void OnTriggerStay2D(Collider2D objeto){
+		if (objeto.tag == "Player") {
+
+
+			conector.enabled = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D objeto){
+		if (objeto.tag == "Player") {
+			
+			conector.enabled = false;
+		}
+	}
+
+
+}
+
+
